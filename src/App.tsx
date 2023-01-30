7f6606657a3a4c9ce5cd8f6549b22969f7951a75
karim-hammami <hammamik790@gmail.com>
2023-01-30 21:39:13 +0100

    testing react-three-fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
+import * as THREE from 'three'
+import * as React from 'react'
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 
-function App() {
+function Box(props: JSX.IntrinsicElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
 
   return (
-    <div className="App">
-        <h1>hello</h1>
-    </div>
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'red'} />
+    </mesh>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  )
+}
+
