c90ed05a61f54f3668940714c577b8af65c79d1c
karim-hammami <hammamik790@gmail.com>
2023-01-31 19:08:56 +0100

    basic box component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,18 @@
-import * as THREE from 'three'
-import * as React from 'react'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas } from "@react-three/fiber";
+import "./style.css"
 
-function Box(props: JSX.IntrinsicElements['mesh']) {
-  const ref = useRef<THREE.Mesh>(null!)
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
-
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'red'} />
+function Box() {
+    return (
+    <mesh>
+       <boxBufferGeometry attach="geometry" />
+       <meshLambertMaterial attach="material" color="hotpink" />
     </mesh>
-  )
+    )        
 }
+
 
 export default function App() {
-  return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+    return <Canvas>
+        <Box />
     </Canvas>
-  )
 }
-
